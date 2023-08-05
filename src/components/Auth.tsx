4dd875cbc20229b3497fea39488353b87e11227e
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-06 00:31:30 +0900

    feat: add auth

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,3 +1,69 @@
+import { useState, FormEvent } from 'react'
+import { CheckBadgeIcon, ArrowPathIcon } from '@heroicons/react/24/solid'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
 export const Auth = () => {
-  return <div>Auth</div>
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [isLogin, setIsLogin] = useState(true)
+  const { loginMutation, registerMutation } = useMutateAuth()
+
+  const submitAuthHandler = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    if (isLogin) {
+      loginMutation.mutate({ email, password })
+      return
+    }
+    await registerMutation
+      .mutateAsync({ email: email, password })
+      .then(() => loginMutation.mutate({ email: email, password }))
+  }
+
+  return (
+    <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 font-mono">
+      <div className="flex items-center">
+        <CheckBadgeIcon className="h-8 w-8 mr-2 text-blue-500" />
+        <span className="text-center text-3xl font-extrabold">
+          Todo app by React/Go(Echo)
+        </span>
+      </div>
+      <h2 className="my-6">{isLogin ? 'Login' : 'Create a new account'}</h2>
+      <form onSubmit={submitAuthHandler}>
+        <div>
+          <input
+            className="mb-3 px-3 text-sm py-2 border border-gray-300"
+            name="email"
+            type="email"
+            autoFocus
+            placeholder="Email address"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+        <div>
+          <input
+            className="mb-3 px-3 text-sm py-2 border border-gray-300"
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+          />
+        </div>
+        <div className="flex justify-center my-2">
+          <button
+            className="disabled:opacity-40 py-2 px-4 rounded text-white bg-indigo-600"
+            disabled={!email || !password}
+            type="submit"
+          >
+            {isLogin ? 'Login' : 'Sign Up'}
+          </button>
+        </div>
+      </form>
+      <ArrowPathIcon
+        onClick={() => setIsLogin((prev) => !prev)}
+        className="h-6 w-6 my-2 text-blue-500 cursor-pointer"
+      />
+    </div>
+  )
 }
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,22 @@
+import { useQueryClient } from '@tanstack/react-query'
+import { ArrowRightOnRectangleIcon } from '@heroicons/react/24/solid'
+import { useMutateAuth } from '../hooks/useMutateAuth'
+
 export const Todo = () => {
-  return <div>Todo</div>
+  const queryClient = useQueryClient()
+  const { logoutMutation } = useMutateAuth()
+
+  const logout = async () => {
+    await logoutMutation.mutateAsync()
+    queryClient.removeQueries(['tasks'])
+  }
+
+  return (
+    <div>
+      <ArrowRightOnRectangleIcon
+        onClick={logout}
+        className="h-6 w-6 my-6 text-blue-500 cursor-pointer"
+      />
+    </div>
+  )
 }
